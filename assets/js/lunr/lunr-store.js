183455ba1ce4a29ef80210f3cb1e528fd4b51fbc
Miluky99 <ming.hu.1980@gmail.com>
2018-11-04 23:40:40 -0500

    test

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,16 +1,54 @@
-var store = [{
-        "title": "Ginger Bread Receipe",
-        "excerpt":"makes about 15 small cookies. Ingredients 175g plain flour 90g brown sugar 50g unsalted butter, diced, at room temperature 2 tbsp golden syrup 1 egg, beaten 1 tsp ground ginger 1 tsp cinnamon 1 tsp bicarbonate of soda Icing sugar to dust Method Sift the flour, ginger, cinnamon and bicarbonate...","categories": [],
-        "tags": [],
-        "url": "/recipes/2018-11-04-ginger-bread-receipe/",
-        "teaser":null},{
-        "title": "自制好吃的泡菜",
-        "excerpt":"Ingredients     大蒜，10瓣   姜   拇指大一小块   梨    半个   洋葱  半个   糯米粉   2大勺+225ml水煮成糊状   鱼露     3大勺   辣椒粉    9大勺   萝卜丝    一杯   韭菜     一杯   糖      1大勺   做法  辣椒酱调好后有淡淡的甜味，不可太甜   白菜腌制6-8小时，* 变软，微透明色，尝起来略咸   萝卜块腌制2-3小时，变软，微透明色，尝起来略   室温放置24小时候放入冰箱发酵20天。  ","categories": [],
-        "tags": [],
-        "url": "/recipes/2018-11-04-pickles/",
-        "teaser":null},{
-        "title": "Chocolate Chip Cookies",
-        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
-        "tags": [],
-        "url": "/recipes/chocolates/",
-        "teaser":null}]
+---
+layout: null
+---
+
+var store = [
+  {%- for c in site.collections -%}
+    {%- if forloop.last -%}
+      {%- assign l = true -%}
+    {%- endif -%}
+    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
+    {%- for doc in docs -%}
+      {%- if doc.header.teaser -%}
+        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
+      {%- else -%}
+        {%- assign teaser = site.teaser -%}
+      {%- endif -%}
+      {
+        "title": {{ doc.title | jsonify }},
+        "excerpt":
+          {%- if site.search_full_content == true -%}
+            {{ doc.content | newline_to_br |
+              replace:"<br />", " " |
+              replace:"</p>", " " |
+              replace:"</h1>", " " |
+              replace:"</h2>", " " |
+              replace:"</h3>", " " |
+              replace:"</h4>", " " |
+              replace:"</h5>", " " |
+              replace:"</h6>", " "|
+            strip_html | strip_newlines | jsonify }},
+          {%- else -%}
+            {{ doc.content | newline_to_br |
+              replace:"<br />", " " |
+              replace:"</p>", " " |
+              replace:"</h1>", " " |
+              replace:"</h2>", " " |
+              replace:"</h3>", " " |
+              replace:"</h4>", " " |
+              replace:"</h5>", " " |
+              replace:"</h6>", " "|
+            strip_html | strip_newlines | truncatewords: 50 | jsonify }},
+          {%- endif -%}
+        "categories": {{ doc.categories | jsonify }},
+        "tags": {{ doc.tags | jsonify }},
+        "url": {{ doc.url | absolute_url | jsonify }},
+        "teaser":
+          {%- if teaser contains "://" -%}
+            {{ teaser | jsonify }}
+          {%- else -%}
+            {{ teaser | absolute_url | jsonify }}
+          {%- endif -%}
+      }{%- unless forloop.last and l -%},{%- endunless -%}
+    {%- endfor -%}
+  {%- endfor -%}]
